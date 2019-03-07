2476d08edb935ad8d5c90aa170ac03b69fb9f81d
Deepak Gupta <deepak.gupta@go-mmt.com>
2019-03-07 15:22:54 +0530

    Changes

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -4,6 +4,13 @@ from django.contrib import admin
 from .models import info
 
 class infoAdmin(admin.ModelAdmin):
-    fields = ['source_arrival_time', 'departure_time']
+    field = ['arrival_time', 'departure_time', 'harness_length','gross_weight', 'atmospheric_pressure',
+                 'room_temperature', 'fuel_capacity_on_left_wing','fuel_quantity_on_left_wing',
+                 'fuel_capacity_on_right_wing', 'fuel_quantity_on_right_wing', 'flight_no']
+    list_display = ('msn_number', 'carrier_name','model','arrival_time','departure_time','source',
+                    'destination','harness_length','gross_weight', 'atmospheric_pressure','room_temperature',
+                    'fuel_capacity_on_left_wing','fuel_quantity_on_left_wing','fuel_capacity_on_right_wing',
+                    'fuel_quantity_on_right_wing', 'flight_no', 'Country')
+    list_filter = ('model', 'carrier_name', 'Country', 'departure_time', 'arrival_time', 'flight_no', 'msn_number', )
 
 admin.site.register(info, infoAdmin)
--- dashboard/models.py
+++ dashboard/models.py
@@ -5,12 +5,16 @@ from django.db import models
 
 class info(models.Model):
     msn_number = models.CharField(max_length=100, db_index=True)
-    carrier_name = models.CharField(max_length=200)
-    capacity = models.CharField(max_length=100, db_index=True)
-    source_arrival_time = models.DateTimeField(db_index=True)
+    carrier_name = models.CharField(max_length=100, null=True, blank=True, db_index=True,
+                                    choices=(('Indigo', 'Indigo'), ('JetAirways', 'JetAirways'),
+                                             ('AirAsia', 'AirAsia'), ('SpiceJet', 'SpiceJet'),
+                                             ('GoAir', 'GoAir')),
+                                    default="India")
+    model = models.CharField(max_length=100, db_index=True)
+    arrival_time = models.DateTimeField(db_index=True)
     departure_time = models.DateTimeField(db_index=True)
-    source = models.CharField(max_length=200)
-    destination = models.CharField(max_length=200)
+    source = models.CharField(max_length=200, verbose_name=('Source Airport'))
+    destination = models.CharField(max_length=200, verbose_name=('Destination Airport'))
     harness_length = models.IntegerField(default=0)
     gross_weight = models.FloatField(max_length=200)
     atmospheric_pressure = models.FloatField(max_length=200)
@@ -20,3 +24,6 @@ class info(models.Model):
     fuel_capacity_on_right_wing = models.FloatField(max_length=200)
     fuel_quantity_on_right_wing = models.FloatField(max_length=200)
     flight_no = models.CharField(max_length=200)
+    Country = models.CharField(max_length=100, null=True, blank=True, db_index=True,
+                                    choices=(('India', 'India'), ('USA', 'USA')),
+                                    default="India")
